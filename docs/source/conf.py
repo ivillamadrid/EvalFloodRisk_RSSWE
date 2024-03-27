40ccbb8757b19be5aea91458fb1136c0e17c4306
ivillamadrid <32260988+ivillamadrid@users.noreply.github.com>
2024-03-27 10:02:50 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'EvalFloodRisk'
+copyright = '2024, ivillanueva.earth'
+author = 'Ignacio Villanueva'
 
 release = '0.1'
 version = '0.1.0'
